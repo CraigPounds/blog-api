9c5782b597630e3e71ef924406de32b14306de8b
Craig Pounds <nweaver@windstream.net>
2018-08-25 17:03:27 -0500

    uncommented tests

--- test/test-blog.js
+++ test/test-blog.js
@@ -30,56 +30,56 @@ describe('Blogs', function() {
         });
       });
   });
-  // it('should add a blog on POST', function() {
-  //   const newBlog = { title: 'AI & Programming', content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit?', author: 'Visual Studio Code', publishDate: 1534473154606 };
-  //   return chai
-  //     .request(app)
-  //     .post('/blog-posts')
-  //     .send(newBlog)
-  //     .then(function(res) {
-  //       expect(res).to.have.status(201);
-  //       expect(res).to.be.json;
-  //       expect(res.body).to.be.a('object');
-  //       expect(res.body).to.include.keys('id', 'title', 'content', 'author', 'publishDate');
-  //       expect(res.body.id).to.not.equal(null);
-  //       expect(res.body).to.deep.equal(
-  //         Object.assign(newBlog, { id: res.body.id })
-  //       );
-  //     });
-  // });
-  // it('should update blog on PUT', function() {
-  //   const updateData = {
-  //     title: 'More and More AI',
-  //     content: 'Shut up human!',
-  //     author: 'Servotron'
-  //   };
-  //   return (
-  //     chai
-  //       .request(app)
-  //       .get('/blog-posts')
-  //       .then(function(res) {
-  //         updateData.id = res.body[0].id;
-  //         return chai
-  //           .request(app)
-  //           .put(`/blog-posts/${updateData.id}`)
-  //           .send(updateData);
-  //       })
-  //       .then(function(res) {
-  //         expect(res).to.have.status(204);
-  //       })
-  //   );
-  // });
-  // it('should delete blog on DELETE', function() {
-  //   return (
-  //     chai
-  //       .request(app)
-  //       .get('/blog-posts')
-  //       .then(function(res) {
-  //         return chai.request(app).delete(`/blog-posts/${res.body[0].id}`);
-  //       })
-  //       .then(function(res) {
-  //         expect(res).to.have.status(204);
-  //       })
-  //   );
-  // });
+  it('should add a blog on POST', function() {
+    const newBlog = { title: 'AI & Programming', content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit?', author: 'Visual Studio Code', publishDate: 1534473154606 };
+    return chai
+      .request(app)
+      .post('/blog-posts')
+      .send(newBlog)
+      .then(function(res) {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.include.keys('id', 'title', 'content', 'author', 'publishDate');
+        expect(res.body.id).to.not.equal(null);
+        expect(res.body).to.deep.equal(
+          Object.assign(newBlog, { id: res.body.id })
+        );
+      });
+  });
+  it('should update blog on PUT', function() {
+    const updateData = {
+      title: 'More and More AI',
+      content: 'Shut up human!',
+      author: 'Servotron'
+    };
+    return (
+      chai
+        .request(app)
+        .get('/blog-posts')
+        .then(function(res) {
+          updateData.id = res.body[0].id;
+          return chai
+            .request(app)
+            .put(`/blog-posts/${updateData.id}`)
+            .send(updateData);
+        })
+        .then(function(res) {
+          expect(res).to.have.status(204);
+        })
+    );
+  });
+  it('should delete blog on DELETE', function() {
+    return (
+      chai
+        .request(app)
+        .get('/blog-posts')
+        .then(function(res) {
+          return chai.request(app).delete(`/blog-posts/${res.body[0].id}`);
+        })
+        .then(function(res) {
+          expect(res).to.have.status(204);
+        })
+    );
+  });
 });
