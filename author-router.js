1410c47ddd5923c384ac62d9e267c03d6dab5fab
Craig Pounds <nweaver@windstream.net>
2018-08-29 18:26:19 -0500

    added POST endpoint for authors collection

--- author-router.js
+++ author-router.js
@@ -18,4 +18,44 @@ router.get('/', (req, res) => {
     });
 });
 
+router.post('/', (req, res) => {
+  const requiredFields = ['firstName', 'lastName', 'userName'];
+  requiredFields.forEach(field => {
+    if(!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  });
+  Author
+    .findOne({ userName: req.body.userName })
+    .then(author => {
+      if (author) {
+        const message = 'Username must be unique';;
+        console.error(message);
+        return res.status(400).send(message);
+      } else {
+        Author
+          .create({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            userName: req.body.userName
+          })
+          .then(author => res.status(201).json({
+            _id: author.id,
+            name: author.serialize(),
+            userName: author.userName
+          }))
+          .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: 'Internal server error'});
+          });
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'Internal server error'});
+    });
+});
+
 module.exports = router;
