36e2f68b34bd17bb259b5c68c879db80379b8b83
Craig Pounds <nweaver@windstream.net>
2018-08-29 20:24:31 -0500

    changed Author, Blog back to const

--- author-router.js
+++ author-router.js
@@ -2,8 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-let { Author } = require('./models');
-const { Blog } = require('./models');
+const { Author, Blog } = require('./models');
 
 router.get('/', (req, res) => {
   Author
