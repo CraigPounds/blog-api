7507c8959e61b7aeae22d6a08a4a28221ebd3f57
Craig Pounds <nweaver@windstream.net>
2018-09-03 08:01:44 -0500

    rebuilt PUT endpoint for authors

--- author-router.js
+++ author-router.js
@@ -31,7 +31,7 @@ router.post('/', (req, res) => {
     .findOne({ userName: req.body.userName })
     .then(author => {
       if (author) {
-        const message = 'Username must be unique';
+        const message = 'Username taken';
         console.error(message);
         return res.status(400).send(message);
       } else {
@@ -58,25 +58,62 @@ router.post('/', (req, res) => {
     });
 });
 
+// router.put('/:id', (req, res) => {
+//   if(!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+//     const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+//     console.error(message);
+//     return res.status(400).json({ message: message });
+//   }
+//   const toUpdate = {};
+//   const updateable = ['firstName', 'lastName', 'userName'];
+//   updateable.forEach(field => {
+//     if(field in req.body) {
+//       toUpdate[field] = req.body[field];
+//     }
+//   });
+//   Author
+//     .findOneAndUpdate(req.params.id, { $set: toUpdate }, { new: true })
+//     .then(author => res.status(204).end())
+//     .catch(err => {
+//       console.error(err);
+//       res.status(500).json({ message: 'Internal server error' });
+//     });
+// });
+
 router.put('/:id', (req, res) => {
-  if(!(req.params.id && req.body.id && req.params.id === req.body.id)) {
-    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
-    console.error(message);
-    return res.status(400).json({ message: message });
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    res.status(400).json({
+      error: 'Request path id and request body id values must match'
+    });
   }
-  const toUpdate = {};
+  const updated = {};
   const updateable = ['firstName', 'lastName', 'userName'];
   updateable.forEach(field => {
-    if(field in req.body) {
-      toUpdate[field] = req.body[field];
+    if (field in req.body) {
+      updated[field] = req.body[field];
     }
   });
+  console.log('testing', req.params.id);
   Author
-    .findOneAndUpdate(req.params.id, { $set: toUpdate }, { new: true })
-    .then(author => res.status(204).end())
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({ message: 'Internal server error' });
+    .findOne({ userName: updated.userName || '', _id: { $ne: req.params.id } })
+    .then(author => {
+      if(author) {
+        const message = 'Username taken';
+        console.error(message);
+        return res.status(400).send(message);
+      }
+      else {
+        Author
+          .findOneAndUpdate(req.params.id, { $set: updated }, { new: true })
+          .then(updatedAuthor => {
+            res.status(200).json({
+              id: updatedAuthor.id,
+              name: `${updatedAuthor.firstName} ${updatedAuthor.lastName}`,
+              userName: updatedAuthor.userName
+            });
+          })
+          .catch(err => res.status(500).json({ message: err }));
+      }
     });
 });
 
--- models.js
+++ models.js
@@ -2,7 +2,9 @@
 
 const mongoose = require('mongoose');
 
-mongoose.set('useCreateIndex', true);
+// mongoose.set('useCreateIndex', true);
+// mongoose.set('useFindAndModify', false);
+
 mongoose.Promise = global.Promise;
 
 const authorSchema = mongoose.Schema({  
