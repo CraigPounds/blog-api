58e32ca2c491ca2da5c907b9dfd21ad538001762
Craig Pounds <nweaver@windstream.net>
2018-09-05 17:15:24 -0500

    changed Authors to Author

--- test/test-blog.js
+++ test/test-blog.js
@@ -5,7 +5,7 @@ const chaiHttp = require('chai-http');
 const expect = chai.expect;
 const faker = require('faker');
 const mongoose = require('mongoose');
-const { Authors, Blog } = require('../models');
+const { Author, Blog } = require('../models');
 const { app, runServer, closeServer } = require('../server');
 const { TEST_DATABASE_URL } = require('../config');
 
@@ -50,15 +50,15 @@ describe('API resource', function() {
     return closeServer();
   });  
   describe('GET authors endpoint', function() {
-    let res;
     it('should return all authors', function() {
+      let res;
       return chai.request(app)
         .get('/authors')
         .then(function(_res) {
           res = _res;
           expect(res).to.have.status(200);
           expect(res.body.authors).to.have.lengthOf.at.least(1);
-          return Authors.count();
+          return Author.count();
         })
         .then(function(count) {
           expect(res.body.authors).to.have.lengthOf(count);
