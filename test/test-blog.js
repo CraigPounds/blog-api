395a56ea219293105a7be1220871bd9accfeff7f
Craig Pounds <nweaver@windstream.net>
2018-09-05 16:33:59 -0500

    added desccribe and it stubs for all endpoints

--- test/test-blog.js
+++ test/test-blog.js
@@ -11,91 +11,180 @@ const { TEST_DATABASE_URL } = require('../config');
 
 chai.use(chaiHttp);
 
-function seedAuthorData() {
-  console.log('seeding author data');
+function seedData() {
+  console.log('seeding data');
+  
+}
+
+function generateAuthorData() {
   return {
-    firstName: faker.firstName,
-    lastName: faker.lastName
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    userName: `${faker.name.firstName()} ${faker.name.lastName()}`
   };
 }
-console.log(seedAuthorData());
-function seedBlogsData() {
-  console.log('seeding blogs data');
 
+function generateBlogData() {
+  return {
+    title: faker.lorem.sentence(),
+    content: faker.lorem.paragraph()    
+  };
 }
-describe('Posts', function() {
+
+function tearDownDb() {
+  console.warn('Deleting database');
+  return mongoose.connection.dropDatabase();
+}
+
+describe('API resource', function() {
   before(function() {
-    runServer();
+    return runServer(TEST_DATABASE_URL);
   });
-  after(function() {
-    closeServer();
+  beforeEach(function() {
+    return seedData();
   });
-  // it('should list blogs on GET', function() {
-  //   return chai
-  //     .request(app)
-  //     .get('/blog-posts')
-  //     .then(function(res) {
-  //       expect(res).to.have.status(200);
-  //       expect(res).to.be.json;
-  //       expect(res.body).to.be.a('array');
-  //       expect(res.body.length).to.be.at.least(1);
-  //       const expectedKeys = ['id', 'title', 'content', 'author', 'publishDate'];
-  //       res.body.forEach(function(blog) { 
-  //         expect(blog).to.be.a('object');
-  //         expect(blog).to.include.keys(expectedKeys);
-  //       });
-  //     });
-  // });
-  // it('should add a blog on POST', function() {
-  //   const newBlog = { title: 'AI & Programming', content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit?', author: 'Visual Studio Code', publishDate: 1534473154606 };
-  //   return chai
-  //     .request(app)
-  //     .post('/blog-posts')
-  //     .send(newBlog)
-  //     .then(function(res) {
-  //       expect(res).to.have.status(201);
-  //       expect(res).to.be.json;
-  //       expect(res.body).to.be.a('object');
-  //       expect(res.body).to.include.keys('id', 'title', 'content', 'author', 'publishDate');
-  //       expect(res.body.id).to.not.equal(null);
-  //       expect(res.body).to.deep.equal(
-  //         Object.assign(newBlog, { id: res.body.id })
-  //       );
-  //     });
-  // });
-  // it('should update blog on PUT', function() {
-  //   const updateData = {
-  //     title: 'More and More AI',
-  //     content: 'Shut up human!',
-  //     author: 'Servotron'
-  //   };
-  //   return (
-  //     chai
-  //       .request(app)
-  //       .get('/blog-posts')
-  //       .then(function(res) {
-  //         updateData.id = res.body[0].id;
-  //         return chai
-  //           .request(app)
-  //           .put(`/blog-posts/${updateData.id}`)
-  //           .send(updateData);
-  //       })
-  //       .then(function(res) {
-  //         expect(res).to.have.status(204);
-  //       })
-  //   );
-  // });
-  // it('should delete blog on DELETE', function() {
-  //   return (
-  //     chai
-  //       .request(app)
-  //       .get('/blog-posts')
-  //       .then(function(res) {
-  //         return chai.request(app).delete(`/blog-posts/${res.body[0].id}`);
-  //       })
-  //       .then(function(res) {
-  //         expect(res).to.have.status(204);
-  //       })
-  //   );
-  // });
+  afterEach(function() {
+    return tearDownDb();
+  });
+  describe('GET authors endpoint', function() {
+    it('should return all authors', function() {
+
+    });
+  });
+  describe('POST authors endpoint', function() {
+    it('should add an author to the authors collection', function() {
+
+    });
+  });
+  describe('PUT authors endpoint', function() {
+    it('should update valid fields for an author by author id', function() {
+
+    });
+  });
+  describe('DELETE authors endpoint', function() {
+    it('should delete author and all associated blogs by author id', function() {
+
+    });
+  });
+  describe('GET blogs endpoint', function() {
+    it('should get all blogs', function() {
+
+    });
+  });
+  describe('GET blogs by id endpoint', function() {
+    it('should get a single blog by blog id', function() {
+
+    });
+  });
+  describe('POST blogs endpoint', function() {
+    it('should add a blog for by author id', function() {
+
+    });
+  });
+  describe('PUT blogs endpoint', function() {
+    it('should update valid fields for a blog by blog id', function() {
+
+    });
+  });
+  describe('DELETE blogs endpoint', function() {
+    it('should delete a single blog by blog id', function() {
+
+    });
+  });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// describe('blog-api resorces', function() {
+//   before(function() {
+//     runServer();
+//   });
+//   after(function() {
+//     closeServer();
+//   });
+//   it('should list blogs on GET', function() {
+//     return chai
+//       .request(app)
+//       .get('/blog-posts')
+//       .then(function(res) {
+//         expect(res).to.have.status(200);
+//         expect(res).to.be.json;
+//         expect(res.body).to.be.a('array');
+//         expect(res.body.length).to.be.at.least(1);
+//         const expectedKeys = ['id', 'title', 'content', 'author', 'publishDate'];
+//         res.body.forEach(function(blog) { 
+//           expect(blog).to.be.a('object');
+//           expect(blog).to.include.keys(expectedKeys);
+//         });
+//       });
+//   });
+//   it('should add a blog on POST', function() {
+//     const newBlog = { title: 'AI & Programming', content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit?', author: 'Visual Studio Code', publishDate: 1534473154606 };
+//     return chai
+//       .request(app)
+//       .post('/blog-posts')
+//       .send(newBlog)
+//       .then(function(res) {
+//         expect(res).to.have.status(201);
+//         expect(res).to.be.json;
+//         expect(res.body).to.be.a('object');
+//         expect(res.body).to.include.keys('id', 'title', 'content', 'author', 'publishDate');
+//         expect(res.body.id).to.not.equal(null);
+//         expect(res.body).to.deep.equal(
+//           Object.assign(newBlog, { id: res.body.id })
+//         );
+//       });
+//   });
+//   it('should update blog on PUT', function() {
+//     const updateData = {
+//       title: 'More and More AI',
+//       content: 'Shut up human!',
+//       author: 'Servotron'
+//     };
+//     return (
+//       chai
+//         .request(app)
+//         .get('/blog-posts')
+//         .then(function(res) {
+//           updateData.id = res.body[0].id;
+//           return chai
+//             .request(app)
+//             .put(`/blog-posts/${updateData.id}`)
+//             .send(updateData);
+//         })
+//         .then(function(res) {
+//           expect(res).to.have.status(204);
+//         })
+//     );
+//   });
+//   it('should delete blog on DELETE', function() {
+//     return (
+//       chai
+//         .request(app)
+//         .get('/blog-posts')
+//         .then(function(res) {
+//           return chai.request(app).delete(`/blog-posts/${res.body[0].id}`);
+//         })
+//         .then(function(res) {
+//           expect(res).to.have.status(204);
+//         })
+//     );
+//   });
+// });
