345f190e4615f3706ed9db5aa906d1c539c090d0
Craig Pounds <nweaver@windstream.net>
2018-09-05 19:26:05 -0500

    finkished authors GET endpoint describe block

--- test/test-blog.js
+++ test/test-blog.js
@@ -11,25 +11,27 @@ const { TEST_DATABASE_URL } = require('../config');
 
 chai.use(chaiHttp);
 
-function seedData() {
-  console.log('seeding data');
-  const authorData = [];
-  const blogData = [];
+function seedAuthorData() {
+  console.info('seeding author data');
+  const seedData = [];
 
   for (let i = 1; i <= 10; i++) {
-    authorData.push(generateAuthorData());
-    blogData.push(generateBlogData());
-  }  
-  Author.insertMany(authorData);
-  return Blog.insertMany(blogData);
+    seedData.push(generateAuthorData());
+  }
+  // console.log(seedData);
+  return Author.insertMany(seedData);
 }
 
 function generateAuthorData() {
   return {
     firstName: faker.name.firstName(),
     lastName: faker.name.lastName(),
-    userName: `${faker.name.firstName()} ${faker.name.lastName()}`
+    userName: gernerateUserName()
   };
+}
+
+function gernerateUserName() {
+  return `${faker.name.firstName()} ${faker.name.lastName()}`;
 }
 
 function generateBlogData() {
@@ -49,14 +51,15 @@ describe('API resource', function() {
     return runServer(TEST_DATABASE_URL);
   });
   beforeEach(function() {
-    return seedData();
+    return seedAuthorData();
   });
   afterEach(function() {
     return tearDownDb();
   });
   after(function() {
     return closeServer();
-  });  
+  }); 
+
   describe('GET authors endpoint', function() {
     it('should return all authors', function() {
       let res;
@@ -66,148 +69,84 @@ describe('API resource', function() {
           res = _res;
           expect(res).to.have.status(200);
           expect(res.body.authors).to.have.lengthOf.at.least(1);
-          return Author.count();
+          return Author.countDocuments();
         })
         .then(function(count) {
           expect(res.body.authors).to.have.lengthOf(count);
         });
     });
-    
+    it('should return authors with correct fields', function() {
+      let resAuthor;
+      return chai.request(app)
+        .get('/authors')
+        .then(function(res) {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body.authors).to.be.a('array');
+          expect(res.body.authors).to.have.lengthOf.at.least(1);
+
+          res.body.authors.forEach(function(author) {
+            expect(author).to.be.a('object');
+            // expect(author).to.include.keys('_id', 'firstName', 'lastName', 'userName');
+            expect(author).to.include.keys('id', 'name', 'userName');
+          });
+          resAuthor = res.body.authors[0];
+          console.log('resAuthor', resAuthor);
+          return Author.findById(resAuthor.id);
+        })
+        .then(function(author) {
+          console.log('author', author);
+          expect(resAuthor.id).to.equal(author.id);
+          expect(resAuthor.userName).to.equal(author.userName);
+        });
+    });
   });
+
   // describe('POST authors endpoint', function() {
   //   it('should add an author to the authors collection', function() {
 
   //   });
   // });
+
   // describe('PUT authors endpoint', function() {
   //   it('should update valid fields for an author by author id', function() {
 
   //   });
   // });
+
   // describe('DELETE authors endpoint', function() {
   //   it('should delete author and all associated blogs by author id', function() {
 
   //   });
   // });
+
   // describe('GET blogs endpoint', function() {
   //   it('should get all blogs', function() {
 
   //   });
   // });
+
   // describe('GET blogs by id endpoint', function() {
   //   it('should get a single blog by blog id', function() {
 
   //   });
   // });
+
   // describe('POST blogs endpoint', function() {
   //   it('should add a blog for by author id', function() {
 
   //   });
   // });
+
   // describe('PUT blogs endpoint', function() {
   //   it('should update valid fields for a blog by blog id', function() {
 
   //   });
   // });
+  
   // describe('DELETE blogs endpoint', function() {
   //   it('should delete a single blog by blog id', function() {
 
   //   });
   // });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// describe('blog-api resorces', function() {
-//   before(function() {
-//     runServer();
-//   });
-//   after(function() {
-//     closeServer();
-//   });
-//   it('should list blogs on GET', function() {
-//     return chai
-//       .request(app)
-//       .get('/blog-posts')
-//       .then(function(res) {
-//         expect(res).to.have.status(200);
-//         expect(res).to.be.json;
-//         expect(res.body).to.be.a('array');
-//         expect(res.body.length).to.be.at.least(1);
-//         const expectedKeys = ['id', 'title', 'content', 'author', 'publishDate'];
-//         res.body.forEach(function(blog) { 
-//           expect(blog).to.be.a('object');
-//           expect(blog).to.include.keys(expectedKeys);
-//         });
-//       });
-//   });
-//   it('should add a blog on POST', function() {
-//     const newBlog = { title: 'AI & Programming', content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit?', author: 'Visual Studio Code', publishDate: 1534473154606 };
-//     return chai
-//       .request(app)
-//       .post('/blog-posts')
-//       .send(newBlog)
-//       .then(function(res) {
-//         expect(res).to.have.status(201);
-//         expect(res).to.be.json;
-//         expect(res.body).to.be.a('object');
-//         expect(res.body).to.include.keys('id', 'title', 'content', 'author', 'publishDate');
-//         expect(res.body.id).to.not.equal(null);
-//         expect(res.body).to.deep.equal(
-//           Object.assign(newBlog, { id: res.body.id })
-//         );
-//       });
-//   });
-//   it('should update blog on PUT', function() {
-//     const updateData = {
-//       title: 'More and More AI',
-//       content: 'Shut up human!',
-//       author: 'Servotron'
-//     };
-//     return (
-//       chai
-//         .request(app)
-//         .get('/blog-posts')
-//         .then(function(res) {
-//           updateData.id = res.body[0].id;
-//           return chai
-//             .request(app)
-//             .put(`/blog-posts/${updateData.id}`)
-//             .send(updateData);
-//         })
-//         .then(function(res) {
-//           expect(res).to.have.status(204);
-//         })
-//     );
-//   });
-//   it('should delete blog on DELETE', function() {
-//     return (
-//       chai
-//         .request(app)
-//         .get('/blog-posts')
-//         .then(function(res) {
-//           return chai.request(app).delete(`/blog-posts/${res.body[0].id}`);
-//         })
-//         .then(function(res) {
-//           expect(res).to.have.status(204);
-//         })
-//     );
-//   });
-// });
